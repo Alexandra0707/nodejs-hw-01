746128ef7e7b3cc8cb02c04f9fb4c8d8714ef5c9
Alexandra0707 <alex07marta@gmail.com>
2025-03-04 11:10:44 +0000

    contacts

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,17 @@
-import { PATH_DB } from '../constants/contacts.js';
+// import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+// export const writeContacts = async (updatedContacts) => {};
+const fs = require('fs').promises;
+const path = require('path');
+
+const contactsPath = path.join(__dirname, '../db/contacts.json');
+
+const writeContacts = async (contacts) => {
+  try {
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  } catch (error) {
+    console.error('Error writing contacts:', error.message);
+  }
+};
+
+module.exports = writeContacts;
