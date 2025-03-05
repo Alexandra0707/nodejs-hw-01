2ab7498b0c1b3400f60826d7451088d8c04928f0
Alexandra0707 <alex07marta@gmail.com>
2025-03-05 12:59:34 +0000

    dfg

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,26 +1,10 @@
-// import { PATH_DB } from '../constants/contacts.js';
-// import fs from 'node:fs/promises';
+import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-// export const readContacts = async () => {
-//   try {
-//     return JSON.parse(await fs.readFile(PATH_DB, 'utf8'));
-//   } catch (err) {
-//     throw `Error reading file:', ${err}`;
-//   }
-// };
-const fs = require('fs').promises;
-const path = require('path');
-
-const contactsPath = path.join(__dirname, '../db/contacts.json');
-
-const readContacts = async () => {
+export const readContacts = async () => {
   try {
-    const data = await fs.readFile(contactsPath, 'utf8');
-    return JSON.parse(data);
-  } catch (error) {
-    console.error('Error reading contacts:', error.message);
-    return [];
+    return JSON.parse(await fs.readFile(PATH_DB, 'utf8'));
+  } catch (err) {
+    throw `Error reading file:', ${err}`;
   }
 };
-
-module.exports = readContacts;
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,17 +1,10 @@
-// import { PATH_DB } from '../constants/contacts.js';
+import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-// export const writeContacts = async (updatedContacts) => {};
-const fs = require('fs').promises;
-const path = require('path');
-
-const contactsPath = path.join(__dirname, '../db/contacts.json');
-
-const writeContacts = async (contacts) => {
+export const writeContacts = async (updatedContacts) => {
   try {
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  } catch (error) {
-    console.error('Error writing contacts:', error.message);
+    await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts), 'utf8');
+  } catch (err) {
+    throw `File writing error:', ${err}`;
   }
 };
-
-module.exports = writeContacts;
